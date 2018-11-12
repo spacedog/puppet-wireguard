a2bf4cc21a243d9114baa06b9c8749ea2a82d123
Marius Schuller <marius.schuller@makandra.de>
2018-11-12 14:49:29 +0100

    Make params.pp compatible with Ubuntu (16.04+)

--- manifests/params.pp
+++ manifests/params.pp
@@ -9,6 +9,13 @@ class wireguard::params {
       $repo_url       = 'https://copr.fedorainfracloud.org/coprs/jdoss/wireguard/repo/epel-7/jdoss-wireguard-epel-7.repo'
       $config_dir     = '/etc/wireguard'
     }
+    'Ubuntu': {
+      $manage_package = true
+      $manage_repo    = true
+      $package_name   = ['wireguard', 'wireguard-dkms', 'wireguard-tools']
+      $repo_url       = 'ppa:wireguard/wireguard'
+      $config_dir     = '/etc/wireguard'
+    }
     default: {
       fail('Unsupported OS family')
     }
