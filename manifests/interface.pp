835ca7305fde48bc91dea46793fcc58568cb42d0
Xand Meaden <xand.meaden@kcl.ac.uk>
2019-05-22 09:46:23 +0100

    allow setting the PersistentKeepalive option for peers

--- manifests/interface.pp
+++ manifests/interface.pp
@@ -22,9 +22,10 @@ define wireguard::interface (
   Enum['present','absent'] $ensure = 'present',
   Optional[Array[Struct[
     {
-      'PublicKey'  => String,
-      'AllowedIPs' => Optional[String],
-      'Endpoint'   => Optional[String],
+      'PublicKey'           => String,
+      'AllowedIPs'          => Optional[String],
+      'Endpoint'            => Optional[String],
+      'PersistentKeepalive' => Optional[Integer],
     }
   ]]]                   $peers        = [],
   Boolean               $saveconfig   = true,
